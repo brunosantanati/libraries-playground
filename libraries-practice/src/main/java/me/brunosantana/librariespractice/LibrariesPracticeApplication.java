2ec65c507fe60f037057fa624b276a580b5cd328
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2023-09-06 13:46:19 -0300

    Empty project

---
+++ libraries-practice/src/main/java/me/brunosantana/librariespractice/LibrariesPracticeApplication.java
@@ -0,0 +1,13 @@
+package me.brunosantana.librariespractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrariesPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrariesPracticeApplication.class, args);
+	}
+
+}
